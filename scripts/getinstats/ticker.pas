03e2ce1501fab0fcc2ba6281cf2d2be10f720b72
harriperhana <harri.huovinen@gmail.com>
2025-05-17 20:21:16 +0300

    initial commit

--- scripts/getinstats/ticker.pas
+++ scripts/getinstats/ticker.pas
@@ -2,12 +2,12 @@ Procedure AppOnIdle(Ticks : Integer);
 Begin
 	If Ticks mod (60 * 1) = 0 Then
 	Begin
-	    SaveMapTimer;
+		SaveMapTimer;
 	End;
 	If Ticks mod (60 * 5) = 0 Then
-    Begin
-        SavePlayersStat;
-        SaveTeamsStat;
-        SaveMapStat;
-    End;
+	Begin
+		SavePlayersStat;
+		SaveTeamsStat;
+		SaveMapStat;
+	End;
 End;
