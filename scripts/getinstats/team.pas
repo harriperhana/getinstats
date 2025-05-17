c9fbd83005942ae9dd369ffce9a8fc4b398cc940
harriperhana <harri.huovinen@gmail.com>
2025-05-17 20:19:36 +0300

    initial commit

--- scripts/getinstats/team.pas
+++ scripts/getinstats/team.pas
@@ -2,7 +2,7 @@ Function SaveTeamsStat() : Boolean;
 Var TeamStat : String;
 Begin
 	TeamStat :=
-	    'id=1' + #13#10 +
+		'id=1' + #13#10 +
 		'players=' + IntToStr(AlphaPlayers) + #13#10 +
 		'score=' + IntToStr(AlphaScore);
 	WriteFile('scripts/getinstats/team/stat/1', TeamStat);
@@ -12,12 +12,12 @@ Begin
 		'score=' + IntToStr(BravoScore);
 	WriteFile('scripts/getinstats/team/stat/2', TeamStat);
 	TeamStat :=
-	    'id=3' + #13#10 +
+		'id=3' + #13#10 +
 		'players=' + IntToStr(CharliePlayers) + #13#10 +
 		'score=' + IntToStr(CharlieScore);
 	WriteFile('scripts/getinstats/team/stat/3', TeamStat);
 	TeamStat :=
-	    'id=4' + #13#10 +
+		'id=4' + #13#10 +
 		'players=' + IntToStr(DeltaPlayers) + #13#10 +
 		'score=' + IntToStr(DeltaScore);
 	WriteFile('scripts/getinstats/team/stat/4', TeamStat);
